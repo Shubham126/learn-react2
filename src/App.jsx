669f3215be8731dfb12279393377a61b8287ccfc
Shubham126 <chromaticshubham26@gmail.com>
2026-05-04 16:48:22 +0000

    useRef and useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
   const [password, setPassword] = useState('');
   const [number, setNumber] = useState(false);
   const [symbol, setSymbol] = useState(false);
+
+  const passwordRef = useRef(null);
+
+  const copyToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
 
  const generatePassword = useCallback(() => {
   let string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -33,7 +40,44 @@ function App() {
     <>
       <h1> this is a password generator</h1>
       <button onClick={generatePassword}>Generate Password</button>
-      <h1>{password}</h1>
+      <div>
+        <input 
+        type='text'
+        value={password}
+        readOnly
+        ref={passwordRef}
+        />
+        <button onClick={copyToClipboard}>Copy</button>
+      </div>
+      
+      <div>
+        <input 
+        type = 'range'
+        min='6'
+        max='99'
+        value={length}
+        onChange={(e) => setLength(Number(e.target.value))}
+        />
+        <p>Length: {length}</p>
+      </div>
+
+      <div>
+        <input
+        type='checkbox'
+        checked={number}
+        onChange= {(e) => setNumber(e.target.checked)}
+        />
+        <p>Include Numbers</p>
+      </div>
+
+      <div>
+        <input
+        type='checkbox'
+        checked={symbol}
+        onChange= {(e) => setSymbol(e.target.checked)}
+        />
+        <p>Include Symbols</p>
+      </div>
     </>
   )
 }
